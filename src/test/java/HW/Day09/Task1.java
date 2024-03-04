d2be3dc50987cdbf6a4457589e95a2b3d00efca0
AmnahAL99 <danaaomar9@gmail.com>
2024-03-04 19:57:56 +0300

    commit

--- src/test/java/HW/Day09/Task1.java
+++ src/test/java/HW/Day09/Task1.java
@@ -5,6 +5,9 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+
+import static org.junit.Assert.assertTrue;
 
 public class Task1 extends TestBase {
 
@@ -23,15 +26,22 @@ public class Task1 extends TestBase {
         WebElement salesButton = driver.findElement(By.xpath("//a[contains(text(),'SALES')]"));
         WebElement creditSideAccount = driver.findElement(By.id("loan")); //  for the CREDIT SIDE Account
         actions.dragAndDrop(salesButton, creditSideAccount).perform();
-      // Perform drag and drop for the first 5000 button
-      //  WebElement first5000Button = driver.findElement(By.xpath("//a[contains(text(),'5000')]"));
-        WebElement first5000Button = driver.findElement(By.cssSelector("section[id='g-container-main'] li:nth-child(2) a:nth-child(1)"));
-        WebElement amount = driver.findElement(By.id("amt8"));
 
-       actions.dragAndDrop(first5000Button, amount).perform();
+        WebElement second5000Button = driver.findElement(By.xpath("(//a[contains(text(),'5000')])[2]"));
+        WebElement creditSideAmount = driver.findElement(By.xpath("//ol[@id='amt8']/li"));
+        actions.dragAndDrop(second5000Button, creditSideAmount).build().perform();
 
+        //Verify the visibility of Perfect text.
+        WebElement perfect = driver.findElement(By.xpath("//a[contains(@class,'button-green')][1]"));
+        Assert.assertTrue(perfect.isDisplayed());
 
 
 
     }
 }
+// Perform drag and drop for the first 5000 button
+//  WebElement first5000Button = driver.findElement(By.xpath("//a[contains(text(),'5000')]"));
+//        WebElement first5000Button = driver.findElement(By.cssSelector("section[id='g-container-main'] li:nth-child(2) a:nth-child(1)"));
+//        WebElement amount = driver.findElement(By.id("amt8"));
+//
+//       actions.dragAndDrop(first5000Button, amount).perform();
